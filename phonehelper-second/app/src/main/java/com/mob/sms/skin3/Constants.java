2caa41320652ae4229cff8798f8e8bb52809ca4b
巴银 <bayin2261@gmail.com>
2021-11-19 16:37:19 +0800

    完成

--- phonehelper-second/app/src/main/java/com/mob/sms/skin3/Constants.java
+++ phonehelper-second/app/src/main/java/com/mob/sms/skin3/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public final static String WX_APPKEY = "51e0b328a07432bad44acb24e6001d1f";
 
     public final static String QQ_APPID = "101952724";
-    public final static String QQ_APPKEY = "a8cdd6efb5a0dd9483cb568071edbbed";
+    public final static String QQ_APPKEY = "c542895394ed3305727295bd56000e21";
 
     public final static String UM_APPID = "6182981be0f9bb492b4b8294";
 }
